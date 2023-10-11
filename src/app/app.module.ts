383ddfdee8435f4defc573b0c43fe7498b24e2e0
denis henry <denysmartin4952@gmail.com>
2023-10-10 20:27:39 -0700

    build/cart, shipping page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,19 +7,30 @@ import { AppComponent } from './app.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { CartComponent } from './cart/cart.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ShippingComponent } from './shipping/shipping.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductAlertsComponent,
     ProductListComponent,
-    TopBarComponent
+    TopBarComponent,
+    ProductDetailsComponent,
+    CartComponent,
+    ShippingComponent
   ],
   imports: [
     BrowserModule,
     ReactiveFormsModule,
+    HttpClientModule,
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
+      { path: 'products/:productId', component: ProductDetailsComponent  },
+      { path: 'cart', component: CartComponent  },
+      { path: 'shipping', component: ShippingComponent   },
     ])
   ],
   providers: [],
